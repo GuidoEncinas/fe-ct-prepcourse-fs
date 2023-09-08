35f5a4cb829275fa49e1cb220c9c99d346e21c92
Guido Encinas <guidoencinasdeveloper@gmail.com>
2023-09-08 11:17:33 -0300

    Se agregaron los homeworks de: arrays, objetos, clases y callbacks

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -6,14 +6,29 @@ function crearGato(nombre, edad) {
    // Además, agregar una propiedad con el nombre "meow".
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
-   // Tu código:
+   
+   let gato = {
+      nombre: nombre,
+      edad: edad,
+      meow: ()=>{
+         return ("Meow!");
+      }
+   }
+
+   return gato;
 }
 
 function nuevoUsuario(nombre, email, password) {
    // Debes crear un nuevo objeto.
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
-   // Tu código:
+   let usuario = {
+      nombre,
+      email,
+      password
+   };
+
+   return usuario;
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -21,58 +36,78 @@ function agregarPropiedad(objeto, propiedad) {
    // Debes agregarle una propiedad con el nombre recibido por parámetro.
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
-   // Tu código:
+   objeto[propiedad] = null;
+
+   return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
    // El parámetro "metodo" es un string que coincide con el nombre de una propiedad del objeto recibido.
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
-   // Tu código:
+   objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
-   // Tu código:
+   let multiplicacion = objetoMisterioso.numeroMisterioso * 5;
+
+   return multiplicacion;
 }
 
 function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
-   // Tu código:
+   delete objeto[propiedad];
+
+   return objeto;
 }
 
 function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar true. Caso contrario, false.
-   // Tu código:
+   if (objetoUsuario.email){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar true. Caso contrario, false.
-   // Tu código:
+  if(objeto.hasOwnProperty(propiedad)){
+   return true;
+  }else{
+   return false;
+  }
 }
 
 function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar true. Caso contrario, false.
-   // Tu código:
+   if (objetoUsuario.password == password){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
    // Reemplaza la contrseña guardada en la propiedad "password" del "objetoUsuario".
    // La nueva contraseña la recibes por parámetro.
    // Retornar el objeto.
-   // Tu código:
+   objetoUsuario.password = nuevaPassword;
+
+   return objetoUsuario;
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
    // El parámetro "objetoUsuario" tiene una propiedad llamada "amigos" igual a un arreglo.
    // Debes agregar el "nuevoAmigo" al final de este arreglo.
    // Retornar el objeto.
-   // Tu código:
+   objetoUsuario.amigos.unshift(nuevoAmigo);
 }
 
 function pasarUsuarioAPremium(objetoMuchosUsuarios) {
@@ -80,15 +115,30 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Cada usuario tiene una propiedad llamada "esPremium".
    // Define esta propiedad de todos los usuarios como true.
    // Retornar el arreglo.
-   // Tu código:
+
+   objetoMuchosUsuarios.forEach(usuario => {
+
+      usuario.esPremium = true;
+      
+   });
+
+   return objetoMuchosUsuarios;
+
 }
+
 
 function sumarLikesDeUsuario(objetoUsuario) {
    // El parámetro "objetoUsuario" tiene una propiedad llamada "posts" que es un arreglo.
    // Este arreglo contiene objetos (post).
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
-   // Tu código:
+   let recuentoLikes = 0;
+   
+   objetoUsuario.posts.forEach(post => {
+      recuentoLikes = recuentoLikes + post.likes;   
+   });
+
+   return recuentoLikes;
 }
 
 function agregarMetodoCalculoDescuento(objetoProducto) {
@@ -101,7 +151,13 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // Precio ---> 10
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
-   // Tu código:
+   objetoProducto.calcularPrecioDescuento = function (){
+      let descuento = this.precio * this.porcenajeDeDescuento;
+
+      let precioFinal = this.precio - descuento;
+
+      return precioFinal;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -4,20 +4,23 @@ function mayuscula(nombre) {
    // Esta función recibe un nombre (string).
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
-   // Tu código:
+
+   nombre = nombre[0].toUpperCase() + nombre.substring(1);
+
+   return nombre;
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
    // En este ejercicio recibirás dos números y un callback.
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
-   // Tu código:
+   return (cb(num1, num2));
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -25,27 +28,62 @@ function sumarArray(arrayOfNumbers, cb) {
    // Suma todos los números del arreglo.
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+
+   let suma = 0;
+   for (let i= 0; i < arrayOfNumbers.length; i++){
+      suma = suma + arrayOfNumbers[i];
+   }
+
+   cb(suma);
 }
 
 function forEach(array, cb) {
    // Recibes un arreglo y un callback.
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
-   // Tu código:
+   array.forEach(element => {
+      cb(element);
+   });
 }
 
 function map(array, cb) {
    // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
-   // Tu código:
+   nuevoArreglo = array.map((element)=>{
+      return cb(element)
+   })
+
+   return nuevoArreglo;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
-   // Tu código:
+   nuevoArreglo = [];
+
+   solucionarYEnviar(arrayOfStrings, crearNuevoArray);
+   
+   function solucionarYEnviar (array, cb){
+      cb(array);
+   }
+
+   function crearNuevoArray(array){
+      
+      for (let i = 0; i < array.length; i++) {
+         
+         let primeraLetra = array[i];
+
+         if (primeraLetra[0] == "a"){
+            nuevoArreglo.push(array[i]);
+         }
+         
+      }
+
+      return nuevoArreglo;
+   }
+
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
